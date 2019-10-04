c088284df8353722d5f9bde778de7c9e98f7dcb4
Victor Fugazzotto <victorfuga@gmail.com>
2019-10-04 14:42:24 -0400

    Vector 2 started

---
+++ EdoEngine/Vector2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Edo
+{
+    [Serializable]
+    public struct Vector2 : IEquatable<Vector2>
+    {
+        // For serialization
+        private float _x;
+        private float _y;
+
+        // Properties for accessing vector values, and Json serialization
+        public float X
+        {
+            get => _x;
+            set => _x = value;
+        }
+
+        public float Y
+        {
+            get => _y;
+            set => _y = value;
+        }
+
+        #region Shorthands
+
+        public static Vector2 Zero => new Vector2(0, 0);
+        public static Vector2 One => new Vector2(1, 1);
+        public static Vector2 Up => new Vector2(0, 1);
+        public static Vector2 Down => new Vector2(0, -1);
+        public static Vector2 Left => new Vector2(-1, 0);
+        public static Vector2 Right => new Vector2(1, 0);
+        public static Vector2 PositiveInfinity => new Vector2(float.PositiveInfinity, float.PositiveInfinity);
+        public static Vector2 NegativeInfinity => new Vector2(float.NegativeInfinity, float.NegativeInfinity);
+
+        #endregion
+
+        public Vector2(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static Vector2 Lerp(Vector2 a, Vector2 b, float t)
+        {
+            t = Mathf.Clamp01(t);
+            return new Vector2(a._x + (b._x - a._x) * t, a._y + (b._y - a._y) * t);
+        }
+
+        public override string ToString() => $"{_x:F1}, {_y:F1}";
+
+        public float Magnitude => (float) Math.Sqrt(_x * _x + _y * _y);
+        public float SqrMagnitude => _x * _x + _y * _y;
+
+        #region IEquatable Stuff
+        
+        public bool Equals(Vector2 other) => _x == other._x && _y == other._y;
+        public override bool Equals(object obj) => obj is Vector2 other && Equals(other);
+
+        // Allows use as keys in hash tables
+        public override int GetHashCode()
+            => unchecked(_x.GetHashCode() * 397 ^ _y.GetHashCode()); // TODO: Performance concerns, hash collisions?
+        
+        #endregion
+
+        #region Operators
+
+        // vector addition
+        public static Vector2 operator +(Vector2 a, Vector2 b) => new Vector2(a._x + b._x, a._y + b._y);
+
+        // vector subtraction
+        public static Vector2 operator -(Vector2 a, Vector2 b) => new Vector2(a._x - b._x, a._y - b._y);
+
+        // vector multiplication
+        public static Vector2 operator *(Vector2 a, Vector2 b) => new Vector2(a._x * b._x, a._y * b._y);
+
+        // vector division
+        public static Vector2 operator /(Vector2 a, Vector2 b) => new Vector2(a._x / b._y, a._y / b._y);
+
+        // negation
+        public static Vector2 operator -(Vector2 v) => new Vector2(-v._x, -v._y);
+
+        // vector-scalar multiplication
+        public static Vector2 operator *(Vector2 v, float s) => new Vector2(v._x * s, v._y * s);
+
+        public static Vector2 operator *(float s, Vector2 v) => new Vector2(v._x * s, v._y * s);
+
+        // vector-scalar division
+        public static Vector2 operator /(Vector2 v, float s) => new Vector2(v._x / s, v._y / s);
+
+        // vector equality
+        public static bool operator ==(Vector2 a, Vector2 b) => a.Equals(b);
+        public static bool operator !=(Vector2 a, Vector2 b) => !a.Equals(b);
+
+        #endregion
+    }
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization.Formatters.Binary;
 using Edo;
 
 namespace Sandbox
@@ -9,19 +10,29 @@ namespace Sandbox
             var client = new Program();
             client.Initialize("vic485", "Sandbox");
             TestJson();
+            TestBinary();
             client.Run();
         }
 
         private static void TestJson()
         {
-            var version = new System.Version(1,0,0,0);
-            var filePath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "version.json");
+            var vec = new Vector2(2, 4);
+            Debug.Log(vec.GetHashCode());
+            var filePath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "vector.json");
             var options = new System.Text.Json.JsonSerializerOptions
             {
                 WriteIndented = true
             };
-            var json = System.Text.Json.JsonSerializer.Serialize(version, options);
+            var json = System.Text.Json.JsonSerializer.Serialize(vec, options);
             System.IO.File.WriteAllText(filePath, json);
+        }
+
+        private static void TestBinary()
+        {
+            var vec = new Vector2(2, 4);
+            var filePath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "vector.dat");
+            using var fs = System.IO.File.Create(filePath);
+            new BinaryFormatter().Serialize(fs, vec);
         }
     }
 }
